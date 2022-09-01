6563d8f4a8e961448409f905cd7ed6cc698bd400
Rahul <63491142+RahulMawa@users.noreply.github.com>
2022-09-01 19:54:05 +0530

    commit file 2

---
+++ bitwise_and_PostFix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+
+    //prefix increment operator:
+    //++i;
+    cout << "++I Output: " << ++i << endl;
+
+    //post fix increment operator:
+    //i++;
+    cout << "I++ output: " << i++ << endl;
+
+    /* EXPLANATION PREFIX AND POSTFIX:
+    So basically using a prefix operator as an argument to a function, the operation will 
+    first take place and it will add it to the value of i and then uses it in the function
+    
+    For post fix, the current value of the variable is used in the function through the 
+    argument, and then the incremented value will be stored in the variable after that 
+    line is executed.
+    */
+
+   //BITWISE OPERATOR:
+   //Basically Logic Gates 
+
+}
